44a922cb905affef560a6c502b52892959300c51
Alexia Donati <donati.alexia@gmail.com>
2025-04-06 20:54:04 +0200

    export the scaler with the model in a sklearn pipeline

--- src/pipeline/components/training.py
+++ src/pipeline/components/training.py
@@ -28,9 +28,8 @@ def training(
     import joblib
     import logging
     from sklearn import preprocessing
-    from sklearn.model_selection import train_test_split
+    from sklearn.pipeline import Pipeline
     from sklearn.ensemble import RandomForestClassifier
-    
 
     df = pd.read_csv(preprocessed_dataset_train.path)
     
@@ -38,23 +37,21 @@ def training(
     X_train = df.drop(columns=["blueWin"]) # Features
     y_train = df["blueWin"] # Labels
     
-    scaler = preprocessing.StandardScaler().fit(X_train)
-    X_train = pd.DataFrame(scaler.transform(X_train), columns=X_train.columns)
-    
-    # Initialize and train the model
-    rf_model = RandomForestClassifier(n_estimators=300,
-                                   min_samples_split=10,
-                                   min_samples_leaf=4,
-                                   max_depth=10,
-                                   criterion='entropy',
-                                   bootstrap=True)
-    
-    rf_model.fit(X_train, y_train)
+    pipeline = Pipeline([
+        ("scaler", preprocessing.StandardScaler()),
+        ("model", RandomForestClassifier(n_estimators=300,
+                                         min_samples_split=10,
+                                         min_samples_leaf=4,
+                                         max_depth=10,
+                                         criterion='entropy',
+                                         bootstrap=True))
+    ])
+    pipeline.fit(X_train, y_train)
     
     # Create the model directory if it doesn't exist
     os.makedirs(model.path, exist_ok=True)
     
     # Save the model
-    joblib.dump(rf_model, os.path.join(model.path, "model.joblib"))
+    joblib.dump(pipeline, os.path.join(model.path, "model.joblib"))
     
     logging.info(f"Model saved to: {model.path}")
